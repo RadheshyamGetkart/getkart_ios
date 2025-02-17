3f92593805e091e344e8e53007ca8f3861f97c25
NareshKumar_getkart <naresh.kumar@getkart.com>
2025-02-17 12:46:26 +0530

    MErging code

--- Getkart/SignIn/LoginView.swift
+++ Getkart/SignIn/LoginView.swift
@@ -8,160 +8,165 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State private var emailPhone: String = ""
-    @State private var path = NavigationPath()
+    @State private var emailPhone: String = "naresh.kumar@getkart.com"
+    @State private var navigateToOTP = false
     var body: some View {
-        NavigationStack(path: $path)  {
+        NavigationView  {
             VStack {
-                HStack{
-                    Spacer()
-                    Button( action: {
-                        
-                    }){
+                
+                // Hidden NavigationLink for the first screen
+                NavigationLink(destination: OTPView(emailPhone: emailPhone), isActive: $navigateToOTP) {
+                                   //EmptyView()
+                               }
+                               
+                ScrollView{
+                    HStack{
+                        Spacer()
+                        Button( action: {
+                            
+                        }){
                             Text("Skip")
-                            .multilineTextAlignment(.center)
-                            .bold()
-                            .font(.system(size: 12.0))
+                                .multilineTextAlignment(.center)
+                                .font(ManropeFont.manrope(.bold, size: 12.0))
+                                .padding(.horizontal)
+                                .frame(width: 80,height: 25)
+                            
+                            
+                        }.foregroundColor(.white)
+                            .background(.orange)
+                            .cornerRadius(12.5)
+                            .padding(.trailing,10)
+                        
+                        
+                        
+                    }
+                    HStack{
+                        Text("Welcome")
+                            .font(ManropeFont.manrope(.bold, size: 20.0))
                             .padding(.horizontal)
-                            .frame(width: 80,height: 25)
-                        
-                        
-                        
-                        
-                    }.foregroundColor(.white)
-                        .background(.orange)
-                        .cornerRadius(12.5)
-                        .padding(.trailing,10)
+                            .frame(height: 50)
+                        Spacer()
+                    }.padding(.top ,100)
                     
-                    
-                    
-                }
-                HStack{
-                    Text("Welcome")
-                        .bold()
-                        .font(.system(size: 20.0))
-                        .padding(.horizontal)
-                        .frame(height: 50)
-                    Spacer()
-                }.padding(.top ,100)
-                
-                HStack {
-                    Text("Login to Getkart")
-                    
-                        .font(.system(size: 15.0))
-                        .padding(.horizontal)
-                    Spacer()
-                }
-                
-                HStack{
-                    Spacer()
-                    TextField("Enter Email or Phone", text: $emailPhone)
-                        .background(.white)
-                        .frame(height: 40)
-                        .padding(5)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.gray, lineWidth: 1)
-                        )
-                        .padding(10)
-                    
-                    
-                    Spacer()
-                }
-                HStack {
-                    Spacer()
-                    Text("0/50")
-                        .padding(.trailing, 20).foregroundColor(.gray)
-                }.padding(.top ,-10)
-                
-                
-                HStack{
-                    Button( action: loginWithEmailPhone){
-                        Text("Continue")
-                            .bold()
-                            .font(.system(size: 15.0))
-                            .frame(width: (UIScreen.main.bounds.size.width - 60),height: 50)
-                            
-                    }.foregroundColor(.white)
-                        .background(.gray)
-                        .cornerRadius(10)
-                        
-                    
-                }
-                
-                HStack{
-                    Text("Don't have an account?")
-                        .font(.system(size: 12.0))
-                        .padding(.top ,10)
-                    Button(action:{
-                        
-                    }){
-                        Text("Sign up")
-                            .font(.system(size: 12.0))
-                            .foregroundColor(.orange)
-                            .underline()
+                    HStack {
+                        Text("Login to Getkart")
+                            .font(ManropeFont.manrope(.regular, size: 15.0))
+                            .padding(.horizontal)
+                        Spacer()
                     }
                     
-                    
-                }
-                HStack {
-                    Text("Or sign in with")
-                        .font(.system(size: 12.0))
-                        .padding(.top ,10)
-                }
-                
-                HStack {
-                    Button(action:{
+                    HStack{
+                        Spacer()
+                        TextField("Enter Email or Phone", text: $emailPhone)
+                            .font(ManropeFont.manrope(.regular, size: 15.0))
+                            .background(.white)
+                            .frame(height: 40)
+                            .padding(5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+                            .padding(10)
                         
-                    }) {
-                        HStack{
-                            Spacer()
-                            Image("login_Google").resizable().frame(width: 30,height: 30)
-                                .padding(.trailing ,10)
-                            Text("Continue with Google")
-                                .foregroundColor(.black)
-                                .frame(height: 50)
+                        
+                        Spacer()
+                    }
+                    HStack {
+                        Spacer()
+                        Text("0/50")
+                            .padding(.trailing, 20).foregroundColor(.gray)
+                    }.padding(.top ,-10)
+                    
+                    
+                    HStack{
+                        Button( action: loginWithEmailPhone){
+                            Text("Continue")
+                                .font(ManropeFont.manrope(.bold, size: 15.0))
+                                
+                                .frame(width: (UIScreen.main.bounds.size.width - 60),height: 50)
                             
-                            
-                            
-                            Spacer()
-                            
-                        }.overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.black, lineWidth: 1))
-                        .padding()
+                        }.foregroundColor(.white)
+                            .background(.gray)
+                            .cornerRadius(10)
                         
                         
                     }
                     
-                }
-                HStack {
-                    Button(action:{
-                        
-                    }) {
-                        HStack{
-                            Spacer()
-                            Image("login_Apple").resizable().frame(width: 30,height: 30)
-                                .padding(.trailing ,10)
-                                .frame(width: 30, height: 30)
-                            Text("Continue with Apple").foregroundColor(.black).frame(height: 50)
+                    HStack{
+                        Text("Don't have an account?")
+                            .font(ManropeFont.manrope(.regular, size: 12.0))
+                            .padding(.top ,10)
+                        Button(action:{
                             
-                            Spacer()
-                            
-                        }.overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.black, lineWidth: 1))
-                        .padding()
+                        }){
+                            Text("Sign up")
+                                .font(ManropeFont.manrope(.regular, size: 12.0))
+                                .foregroundColor(.orange)
+                                .underline()
+                        }
                         
                         
                     }
+                    HStack {
+                        Text("Or sign in with")
+                            .font(ManropeFont.manrope(.regular, size: 12.0))      .padding(.top ,10)
+                    }
                     
+                    HStack {
+                        Button(action:{
+                            
+                        }) {
+                            HStack{
+                                Spacer()
+                                Image("login_Google").resizable().frame(width: 30,height: 30)
+                                    .padding(.trailing ,10)
+                                Text("Continue with Google")
+                                    .font(ManropeFont.manrope(.regular, size: 15.0))
+                                    .foregroundColor(.black)
+                                    .frame(height: 50)
+                                
+                                
+                                
+                                Spacer()
+                                
+                            }.overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 1))
+                            .padding()
+                            
+                            
+                        }
+                        
+                    }
+                    HStack {
+                        Button(action:{
+                            
+                        }) {
+                            HStack{
+                                Spacer()
+                                Image("login_Apple").resizable().frame(width: 30,height: 30)
+                                    .padding(.trailing ,10)
+                                    .frame(width: 30, height: 30)
+                                Text("Continue with Apple")
+                                    .font(ManropeFont.manrope(.regular, size: 15.0))
+                                    .foregroundColor(.black).frame(height: 50)
+                                
+                                Spacer()
+                                
+                            }.overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 1))
+                            .padding()
+                            
+                            
+                        }
+                        
+                    }
                 }
-                
                 Spacer()
                 HStack {
                     Text("By Signning Up/Logging in, you agree to our")
-                        .font(.system(size: 10.0))
+                        .font(ManropeFont.manrope(.regular, size: 10.0))
                         .padding(.horizontal)
                 }
                 
@@ -171,20 +176,20 @@ struct LoginView: View {
                         
                     }){
                         Text("Terms of Service")
-                            .font(.system(size: 10.0))
+                            .font(ManropeFont.manrope(.regular, size: 12.0))
                             .foregroundColor(.orange)
                             .underline()
                     }
                     
                     Text("and")
-                        .font(.system(size: 10.0))
+                        .font(ManropeFont.manrope(.regular, size: 12.0))
                     
                     
                     Button(action:{
                         
                     }){
                         Text("Privacy Policy")
-                            .font(.system(size: 10.0))
+                            .font(ManropeFont.manrope(.regular, size: 12.0))
                             .foregroundColor(.orange)
                             .underline()
                     }
@@ -194,24 +199,20 @@ struct LoginView: View {
                 
             }
             .padding()
-            .navigationDestination(for: String.self) { value in
-                            if value == "otpView" {
-                                OTPView(emailPhone: emailPhone)
-                            }
-                        }
+            
         }
     }
     func loginWithEmailPhone(){
         print("Text entered", $emailPhone.wrappedValue)
         if validatePhoneNumber($emailPhone.wrappedValue) == true {
             print("Login via Phone")
-            path.append("otpView")
+            navigateToOTP = true
         }else if validateEmail( $emailPhone.wrappedValue) == true {
             print("Login via Email")
-            path.append("otpView")
+            navigateToOTP = true
         }else {
             print("Enter validEmail or Phone")
-            path.append("otpView")
+            navigateToOTP = false
         }
     }
     
