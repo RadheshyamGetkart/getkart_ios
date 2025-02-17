88a7e26b11e8f60246a5407637989a9de7c66107
Radhe@123 <radheshyam.getkart@gmail.com>
2025-02-17 11:50:37 +0530

    Merge code

--- Getkart/SignUp/SignUpView.swift
+++ Getkart/SignUp/SignUpView.swift
@@ -12,25 +12,35 @@ struct SignUpView: View {
     var body: some View {
         
         VStack{
-            HStack{
-                Spacer()
-                Button {
-                    
-                } label: {
-                    
-                    Text("Skip").frame(width: 100,height: 30).foregroundColor(Color(hex: " #f8b8af")).overlay(
-                        RoundedRectangle(cornerRadius: 15).stroke(Color(hex: "#f6e7e9"), lineWidth: 2)
-                    )
+            ScrollView{
+                HStack{
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        
+                        Text("Skip").font(ManropeFont.manrope(.medium, size: 18.0)).frame(width: 100,height: 35).foregroundColor(Color(hex: " #fa7860"))
+                        
+                    }.background(Color(hex: "#f6e7e9")).cornerRadius(17.5).padding(10)
                 }
-                
-                Text("Welcome")
-                Text("Sign up")
-
-                
+                VStack{
+                    HStack{
+                        Text("Welcome").multilineTextAlignment(.leading).font(ManropeFont.manrope(.extraBold, size: 22.0)).padding(.trailing,10)
+                        Spacer()
+                    }.padding(5)
+                    HStack{
+                        
+                        Text("Sign up").multilineTextAlignment(.leading).font(ManropeFont.manrope(.regular, size: 18.0)).padding(.trailing,10)
+                        Spacer()
+                    }.padding(5)
+                }.padding([.leading,.trailing],15)
+                Spacer()
+            }
+          
             }
         }
     }
-}
+
     
 
 #Preview {
--- Getkart/Utility.swift
+++ Getkart/Utility.swift
@@ -8,6 +8,45 @@
 import Foundation
 import SwiftUICore
 
+
+enum ManropeFont {
+   
+    case semiBold
+    case bold
+    case medium
+    case regular
+    case extraBold
+    
+    case custom(String)
+    
+    var value: String {
+        
+        switch self {
+       
+        case .semiBold:
+            return "Manrope-SemiBold"
+            
+        case .custom(let name):
+            return name
+        case .bold:
+            return "Manrope-Bold"
+        case .medium:
+            return "Manrope-Medium"
+        case .regular:
+            return "Manrope-Regular"
+        case .extraBold:
+            return "Manrope-ExtraBold"
+        }
+    }
+    
+    static func manrope(_ type: ManropeFont, size: CGFloat = 16) -> Font {
+        return .custom(type.value, size: size)
+    }
+    
+}
+
+
+
  extension Color {
      
     init(hex: String) {
